fe1ea5c435ebb73e6520a17149f4ec797676394b
sumit.neupane001 <sumit.neupane2062@gmail.com>
2026-07-01 15:32:38 +0545

    Updated

--- DelagateandEvent/Program.cs
+++ DelagateandEvent/Program.cs
@@ -1,19 +1,23 @@
-﻿//delegate
+﻿
+// Delegate
 delegate void Notification(string message);
 
-//Event
+// Event
 class Publisher
 {
     public event Notification? MessageSent;
+
     public void Send(string message)
     {
         MessageSent?.Invoke(message);
     }
 }
-//Generic
+
+// Generic Class
 public class Box<T>
 {
     public T Value { get; set; }
+
     public void Display()
     {
         Console.WriteLine($"Value: {Value}");
@@ -22,37 +26,82 @@ public class Box<T>
 
 class MyClass
 {
-
     public static void Main()
     {
-        Console.WriteLine("====Delegate and Event====");
+        // Delegate and Event
+        Console.WriteLine("==== Delegate and Event ====");
         Publisher publisher = new Publisher();
 
         publisher.MessageSent += ReceiveMessage;
         publisher.Send("Hello, World!");
 
-        Console.WriteLine("===Generic===");
-        Console.WriteLine("===List of int====");
+        // Generic Class
+        Console.WriteLine("\n==== Generic Class ====");
         Box<int> intBox = new Box<int> { Value = 42 };
         intBox.Display();
 
-        Console.WriteLine("===List of string====");
         Box<string> stringBox = new Box<string> { Value = "Hello, World!" };
         stringBox.Display();
 
-        Console.WriteLine("===list of Generic====");
-        List<string> students =new List<string>();
+        // Generic List
+        Console.WriteLine("\n==== Generic List ====");
+        List<string> students = new List<string>();
+
         students.Add("Sumit");
         students.Add("Bob");
         students.Add("John");
-        foreach (var student in students)
+
+        foreach (string student in students)
         {
-            Console.WriteLine($"Student: {student}");
+            Console.WriteLine(student);
+        }
+
+        // ArrayList
+        Console.WriteLine("\n==== ArrayList ====");
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.Add(100);
+        arrayList.Add("Sumit");
+        arrayList.Add(99.5);
+        arrayList.Add(true);
+
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Dictionary
+        Console.WriteLine("\n==== Dictionary ====");
+        Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+        dictionary.Add(1, "Sumit");
+        dictionary.Add(2, "Bob");
+        dictionary.Add(3, "John");
+
+        foreach (KeyValuePair<int, string> item in dictionary)
+        {
+            Console.WriteLine($"Roll: {item.Key}, Name: {item.Value}");
+        }
+
+        // Stack
+        Console.WriteLine("\n==== Stack ====");
+        Stack<string> stack = new Stack<string>();
+
+        stack.Push("First");
+        stack.Push("Second");
+        stack.Push("Third");
+
+        Console.WriteLine("Top Element: " + stack.Peek());
+
+        Console.WriteLine("Stack Elements:");
+        while (stack.Count > 0)
+        {
+            Console.WriteLine(stack.Pop());
         }
     }
+
     public static void ReceiveMessage(string message)
     {
-        Console.WriteLine($"Subscriber message: {message}");
+        Console.WriteLine($"Subscriber Message: {message}");
     }
-
 }
