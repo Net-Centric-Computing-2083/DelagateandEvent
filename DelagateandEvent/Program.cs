079827c33af30a3fcc55cbed0e6061e5e4ea2892
aakritidhungana <aakritidhungana123@gmail.com>
2026-07-01 21:18:06 +0545

    Dictionary and Stack

--- DelagateandEvent/Program.cs
+++ DelagateandEvent/Program.cs
@@ -1,4 +1,9 @@
-﻿//delegate
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+//delegate
 delegate void Notification(string message);
 
 //Event
@@ -48,6 +53,46 @@ class Program
         foreach(var student in students)
            {
             Console.WriteLine($"Student: {student}");
+        }
+
+
+        //array list
+        Console.WriteLine("\n===== ArrayList =====");
+
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.Add(100);
+        arrayList.Add("Aakriti");
+        arrayList.Add(95.5);
+
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+
+        //dictionary
+        Console.WriteLine("==Dictionary==");
+        Dictionary<int, string> studentDictionary = new Dictionary<int, string>();
+        studentDictionary.Add(1, "Aakriti");
+        studentDictionary.Add(2, "Aaku");
+        foreach (var item in studentDictionary)
+        {
+            Console.WriteLine($"Student ID: {item.Key}, Name: {item.Value}");
+        }
+
+        //stack
+        Console.WriteLine("==Stack==");
+
+        Stack<string> subjectStack = new Stack<string>();
+        subjectStack.Push("Maths");
+        subjectStack.Push("Science");
+        Console.WriteLine("Top of the stack: " + subjectStack.Peek());
+        Console.WriteLine("Popped from the stack: " + subjectStack.Pop());
+        Console.WriteLine("Top of the stack after pop: " + subjectStack.Peek());
+
+        foreach (var subject in subjectStack)
+        {
+            Console.WriteLine($"Subject: {subject}");
         }
     }
     public static void ReceiveMessage(string message)
