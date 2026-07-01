fdfe5f2e32247151865bc9a457d390eceb16ec4e
ishakarki777 <isha4karki@gmail.com>
2026-07-01 08:09:12 +0545

    update delegate demo

--- DelagateandEvent/Program.cs
+++ DelagateandEvent/Program.cs
@@ -1,2 +1,59 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Delagate");
+﻿//delegate
+delegate void Notification(string message);
+
+//Event
+class Publisher
+{
+    public event Notification? MessageSent;
+
+    public void Send(string message)
+    {
+        MessageSent?.Invoke(message);
+    }
+}
+//Generic
+public class Box<T>
+{
+    public T Value { get; set; }
+    public void Display()
+    {
+        Console.WriteLine($"Value : {Value}");
+    }
+}
+
+class Program
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("--Delegate and Event--");
+        Publisher publisher = new Publisher();
+
+        publisher.MessageSent += ReceiveMessage;
+        publisher.Send("Hello World");
+
+        Console.WriteLine("---Generic---");
+        Console.WriteLine("---List of integer---");
+        Box<int> intBox = new Box<int> { Value = 42 };
+        intBox.Display();
+
+        Console.WriteLine("---List of string---");
+        Box<string> stringBox = new Box<string> { Value = "Hello World!" };
+        stringBox.Display();
+
+        Console.WriteLine("---List of Generic---");
+        List<string> students = new List<string>();
+        students.Add("Alice");
+        students.Add("Bob");
+        students.Add("Charlie");
+
+        foreach (var student in students) {
+            Console.WriteLine($"Student: {student}");
+        }
+    }
+    public static void ReceiveMessage(string message)
+    {
+        Console.WriteLine($"Subscriber message: {message}");
+    }
+    //Generic 
+}
