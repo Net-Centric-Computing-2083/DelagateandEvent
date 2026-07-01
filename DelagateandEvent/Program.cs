33e5a25899b155fab2ed1fba643bbc41642fec1f
kushalpahadi <kushalpahadi132@gmail.com>
2026-07-01 08:01:00 +0545

    7/1/2026___ Delegate and Event and Generic

--- DelagateandEvent/Program.cs
+++ DelagateandEvent/Program.cs
@@ -1,2 +1,63 @@
-﻿Console.WriteLine("Delegate and Events");
+﻿Console.WriteLine("Delegate and Events and Generic");
 Console.WriteLine("Kushal80010932");
+//delegate 
+delegate void Notification(string message);
+
+
+//event
+class Publisher
+{
+    public event Notification? MessageSent;
+
+
+    public void Send(string message)
+    {
+        MessageSent?.Invoke(message);
+    }
+
+}
+
+//Generic class
+
+public class Box<T>{
+    public T Value { get; set; }
+    public void Display()
+    {
+        Console.WriteLine($"Value : {Value} ");
+    }
+}
+
+
+
+class MyClass
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("========DELEGATE AND EVENTS=======");
+        Publisher publisher = new Publisher();
+        publisher.MessageSent += RecieveMessage;
+        publisher.Send("Hello, Kushal! The event has been fired.");
+
+
+        //generic
+        Console.WriteLine("==========Generic ========");
+        Console.WriteLine("==========List of Integer ========");
+
+        Box<int>  intBox = new Box<int> { Value = 42 };
+        intBox.Display();
+
+
+        Console.WriteLine("==========List of String========");
+
+        Box<string> stringBox = new Box<string> { Value = "Kushal" };
+        stringBox.Display();
+    }
+
+    public static void RecieveMessage(string message) {
+        Console.WriteLine($"Subscriber message: {message}");
+            }
+
+
+
+}
