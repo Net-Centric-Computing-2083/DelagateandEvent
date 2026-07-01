88cc4e58f1f604df83a05141853577121de013ea
gayatri <gayatrijoshi2063@gmail.com>
2026-07-01 21:02:50 +0545

    update delegate

--- DelagateandEvent/Program.cs
+++ DelagateandEvent/Program.cs
@@ -1,5 +1,8 @@
-﻿//delegate 
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
+//delegate 
 delegate void Notification(string message);
 
 //Event 
@@ -7,11 +10,11 @@ class Publisher
 {
     public event Notification? MessageSent;
     public void Send(string message)
-
     {
         MessageSent?.Invoke(message);
     }
 }
+
 //Generic
 public class Box<T>
 {
@@ -25,10 +28,11 @@ public class Box<T>
 class Program
 {
     public static void Main()
+
     {
         Console.WriteLine("====Delegate and Event====");
     
-Publisher publisher = new Publisher();
+        Publisher publisher = new Publisher();
 
         publisher.MessageSent += ReceiveMessage;
         publisher.Send("Hello, World!");
@@ -38,7 +42,6 @@ Publisher publisher = new Publisher();
 
         Box<int> intBox = new Box<int> { Value = 42 };
         intBox.Display();
-
 
         Console.WriteLine("===List of string");
         Box<string> stringBox = new Box<string> { Value = "Hello, world!" };
@@ -53,6 +56,54 @@ Publisher publisher = new Publisher();
         {
             Console.WriteLine($"Student: {student}");
         } 
+
+        // Array
+        Console.WriteLine("==== ArrayList ====");
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add(100);
+        arrayList.Add("Hello");
+        arrayList.Add(45.5);
+        arrayList.Add(true);
+
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+
+        //Dictionary
+        Console.WriteLine("\n==== Dictionary ====");
+
+        Dictionary<int, string> studentsDict = new Dictionary<int, string>();
+
+        studentsDict.Add(1, "Alice");
+        studentsDict.Add(2, "Bob");
+        studentsDict.Add(3, "Charlie");
+
+        foreach (KeyValuePair<int, string> student in studentsDict)
+        {
+            Console.WriteLine($"ID: {student.Key}, Name: {student.Value}");
+        }
+
+        //Stack
+
+        Console.WriteLine("\n==== Stack ====");
+
+        Stack<string> stack = new Stack<string>();
+
+        stack.Push("Book");
+        stack.Push("Pen");
+        stack.Push("Notebook");
+
+        Console.WriteLine("Top Item: " + stack.Peek());
+
+        Console.WriteLine("Removing: " + stack.Pop());
+
+        Console.WriteLine("Remaining Items:");
+        foreach (string item in stack)
+        {
+            Console.WriteLine(item);
+        }
     }
 
     public static void ReceiveMessage(string message)
@@ -60,4 +111,3 @@ Publisher publisher = new Publisher();
         Console.WriteLine($"Subscriber message:{message}");
     }
 }
-
