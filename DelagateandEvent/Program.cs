bfa30d24036725f1aaed4bc35515e9c9e66462d8
Amrita601-pixel <kushwahaamrita222@gmail.com>
2026-07-02 00:08:33 +0545

    Array list,Dictionary and Stack

--- DelagateandEvent/Program.cs
+++ DelagateandEvent/Program.cs
@@ -1,4 +1,5 @@
 ﻿//delegate
+using System.Collections;
 using System.Net.Sockets;
 
 delegate void Notificarion(string message);
@@ -46,8 +47,10 @@ class Program
         Box<string> stringBox = new Box<string> { Value = "Hello, World!" };
         stringBox.Display();
 
+        //List
+
         Console.WriteLine("=====List of Generic=====");
-        List<string> students =new List<string>();
+        List<string> students = new List<string>();
         students.Add("Alice");
         students.Add("Amrita");
         students.Add("Susma");
@@ -57,7 +60,59 @@ class Program
             Console.WriteLine($"student:{student}");
         }
 
+        // ArrayList
+        Console.WriteLine("\n=====ArrayList Collection=====");
+        ArrayList items = new ArrayList();
+
+        items.Add(100);
+        items.Add("Apple");
+        items.Add(50.5);
+
+        foreach (var item in items)
+        {
+            Console.WriteLine($"Item: {item}");
+        }
+
+        // Dictionary
+        Console.WriteLine("\n=====Dictionary Collection=====");
+        Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+        dictionary.Add(1, "Alice");
+        dictionary.Add(2, "Amrita");
+        dictionary.Add(3, "Susma");
+
+        foreach (var item in dictionary)
+        {
+            Console.WriteLine($"Key: {item.Key}  Value: {item.Value}");
+        }
+
+
+
+        // Stack
+        Console.WriteLine("\n=====Stack Collection=====");
+        Stack<string> stack = new Stack<string>();
+
+        stack.Push("Book");
+        stack.Push("Pen");
+        stack.Push("Copy");
+
+        foreach (var item in stack)
+        {
+            Console.WriteLine($"Item: {item}");
+        }
+
+        Console.WriteLine($"Top Item: {stack.Peek()}");
+
+        stack.Pop();
+
+        Console.WriteLine("After Pop:");
+        foreach (var item in stack)
+        {
+            Console.WriteLine($"Item: {item}");
+        }
     }
+        
+    
 
     public static void ReceiveMessage(string message)
     {
