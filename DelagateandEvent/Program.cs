d565c48c1dfe8ffa712a67bf44fe89c3600a4f30
Saurav Basnet <misterbasnet@gmail.com>
2026-07-01 07:18:25 +0545

    changes

--- DelagateandEvent/Program.cs
+++ DelagateandEvent/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Animal
+{
+    public string Name { get; set; }
+    public Animal(string name)
+    {
+        Name = name;
+    }
+    public void Speak()
+    {
+        Console.WriteLine($"{Name} makes a sound.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal dog = new Animal("Dog");
+        dog.Speak();
+    }
+}
